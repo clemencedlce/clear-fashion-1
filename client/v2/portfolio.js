c48e2d41c9da4eb446041df51c10f8162d49c120
clemencedlce <117808800+clemencedlce@users.noreply.github.com>
2023-04-02 20:14:21 +0200

    Update portfolio.js

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -4,22 +4,22 @@
 /*
 Description of the available api
 GET https://clear-fashion-api.vercel.app/
-
 Search for specific products
-
 This endpoint accepts the following optional query string parameters:
-
 - `page` - page of products to return
 - `size` - number of products to return
-
 GET https://clear-fashion-api.vercel.app/brands
-
 Search for available brands list
 */
 
 // current products on the page
 let currentProducts = [];
 let currentPagination = {};
+let brandsCount = 0;
+let p50 = 0;
+let p90 = 0;
+let p95 = 0;
+
 
 // instantiate the selectors
 const selectShow = document.querySelector('#show-select');
@@ -27,7 +27,10 @@ const selectPage = document.querySelector('#page-select');
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
 const selectBrand=document.querySelector("#brand-select");
-const selectRecent= document.querySelector('#recent-select');
+const recentlyReleased = document.querySelector("#recently-released");
+const reasonablePrice = document.querySelector("#reasonable-price");
+const sortproduct = document.querySelector("#sort-select");
+const spanNbProductsnew = document.querySelector('#nbNewProducts');
 
 /**
  * Set global value
@@ -43,9 +46,10 @@ const setCurrentProducts = ({result, meta}) => {
  * Fetch products from api
  * @param  {Number}  [page=1] - current page to fetch
  * @param  {Number}  [size=12] - size of the page
+ * @param brand - all : display all the brands 
  * @return {Object}
  */
-const fetchProducts = async (page = 1, size = 12, brand ="all") => {
+const fetchProducts = async (page = 1, size = 12, brand ="all", sort_type="price-asc", filter_recent = false, filter_price = false) => {
   try {
     const response = await fetch(
       `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`+ (brand !== "all" ? `&brand=${brand}` : "")
@@ -56,9 +60,54 @@ const fetchProducts = async (page = 1, size = 12, brand ="all") => {
       console.error(body);
       return {currentProducts, currentPagination};
     }
+    
+    var result=body.data.result;
+    console.log(Array.isArray(result));
       
+    console.log(Array.isArray(result));
+    if (filter_recent){
+      result = result.filter(product => (new Date() - new Date(product.released)) / (1000 * 60 * 60 * 24) < 50);
+    }
+    if (filter_price){  
+      result=result.filtre(product=>product.price<50);
+    }
+    
+    var meta = {
+      currentPage: page,
+      pageCount: Math.ceil(result.length / size),
+      pageSize: size,
+      count: result.length
+    };
+      
+    /** Number of brands **/ 
+    if(result.length > 0){
+      result.reduce((acc, product) => {
+        if(!acc[product.brand]) {
+          acc[product.brand] = 1;
+          brandsCount++;
+        }
+        return acc;
+      }, {});
+    };
+    
+    /** Feature 10 : p50, p90 and p95 price value indicator**/ 
+    if(result.length > 0)
+    {
+      p50 = [...result].sort((a, b) => a.price - b.price)[Math.floor(result.length / 2)].price;
+      p90 = [...result].sort((a, b) => a.price - b.price)[Math.floor(result.length * 0.9)].price;
+      p95 = [...result].sort((a, b) => a.price - b.price)[Math.floor(result.length * 0.95)].price;
+    }
+    else
+    {
+      p50 = 0;
+      p90 = 0;
+      p95 = 0;
+    }
 
-    return body.data;
+     
+    return {result,meta};
+
+    /*return body.data;*/ 
   } catch (error) {
     console.error(error);
     return {currentProducts, currentPagination};
@@ -105,6 +154,7 @@ const renderProducts = products => {
         <span>${product.brand}</span>
         <a href="${product.link}">${product.name}</a>
         <span>${product.price}</span>
+        <span>${product.released}</span>
       </div>
     `;
     })
@@ -132,8 +182,39 @@ const renderPagination = pagination => {
   selectPage.selectedIndex = currentPage - 1;
 };
 
+/** Render Brand selector
+* @param {Array} brands
+*/ 
+
+const renderBrands = brands => {
+    brands =  [...new Set(currentProducts.map(product => product.brand))];
+    const options = brands
+    .map(brand => `<option value="${brand}">${brand}</option>`)
+    .join('');
+    
+    selectBrand.innerHTML = `<option value="">All brands</option>${options}`;
+}
 
 
+/** Render Brands Count 
+*/ 
+
+const renderBrandsCount = () => {
+  const spanNbBrands = document.querySelector('#nbBrands');
+  spanNbBrands.innerHTML = brandsCount;
+};
+
+/** Render p50, p90, p95 
+*/ 
+
+const renderStats = () => {
+  const spanP50 = document.querySelector('#spanP50');
+  spanP50.innerHTML = p50;
+  const spanP90 = document.querySelector('#spanP90');
+  spanP90.innerHTML = p90;
+  const spanP95 = document.querySelector('#spanP95');
+  spanP95.innerHTML = p95;
+};
 
 /**
  * Render page selector
@@ -141,21 +222,19 @@ const renderPagination = pagination => {
  */
 const renderIndicators = pagination => {
   const {count} = pagination;
-
+  
+  /** Feature 8 : Number of products **/ 
   spanNbProducts.innerHTML = count;
     
-    const brands = [...new Set(currentProducts.map(product => product.brand))];
-  const options = brands
-    .map(brand => `<option value="${brand}">${brand}</option>`)
-    .join('');
-
-  selectBrand.innerHTML = `<option value="">All brands</option>${options}`;
+  
 };
 
 const render = (products, pagination) => {
   renderProducts(products);
   renderPagination(pagination);
   renderIndicators(pagination);
+  renderBrands(brand);
+  renderBrandsCount();
 };
 
 
@@ -188,9 +267,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   render(currentProducts, currentPagination);
 });
 
-/** Feature 2 **/ 
-
-
+/** Feature 2 : filter product by brands **/ 
 
 const filterProductsByBrand = brand => {
   const filteredProducts = currentProducts.filter(product => product.brand === brand);
@@ -208,6 +285,70 @@ selectBrand.addEventListener('change', async (event) => {
 
 });
 
+/** Feature 3 : filter products by Recent products 
+A recent product is less than 2 weeks
+**/
 
 
+if (recentlyReleased){
+  recentlyReleased.addEventListener('change',async (event)=>{
+  const products=await fetchProducts(currentPagination.currentPage,currentPagination.pageCount,selectBrand.value,event.target.checked,reasonablePrice.checked,sortproduct);
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});}
+
+
+
+/** Feature 4 : Filter by reasonable price 
+A reasonable price is less than 50$
+**/
+
+if (reasonablePrice){
+  reasonablePrice.addEventListener('change',async (event)=>{
+  const products=await fetchProducts(currentPagination.currentPage,currentPagination.pageCount,selectBrand.value,recentlyReleased.checked,event.target.checked,sortproduct);
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});}
+
+/** Feature 5 : Sort by Price  
+        ** AND **
+/** Feature 6 : sort by date **/ 
+
+const sortProducts = (sortOption) => {
+  let sortedProducts=[...currentProducts];
+
+  switch(sortOption) {
+    case 'price-asc':
+      sortedProducts.sort((a,b) => a.price - b.price);
+      break;
+    case 'price-desc':
+      sortedProducts.sort((a,b) => b.price - a.price);
+      break;
+    
+    case 'date-asc':
+        sortedProducts.sort((a,b) => a.released - b.released);
+       
+        break;
+      case 'date-desc':
+        sortedProducts.sort((a,b) => b.released - a.released);
+          break;
+        default:
+          break;
+  }
+  render(sortedProducts, {currentPage: 1, count: sortedProducts.length, pageCount: 1})
+};
+
+const selectSort=document.querySelector('#sort-select');
+
+
+selectSort.addEventListener('change', async (event) => {
+  const selectedSort = selectSort.value;
+  sortedProduct=sortProducts(selectedSort);
+  products.result=sortedProduct.result;
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+/** Feature 9 : number of recent products indicator **/
 
