8e07cf00509c3832fa1998db4998fc728b896b03
clemencedlce <117808800+clemencedlce@users.noreply.github.com>
2023-04-02 23:02:31 +0200

    feat(shop): scrape new products

---
+++ server/Products.js
@@ -0,0 +1,480 @@
+[
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Butterfly Oat White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Kramer Black",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Shirt Marstrand Butterfly Oat White",
+      "price": 89
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Seinfeld Family White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm No Soup White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Newman White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm George Black",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm All Out Boat Off-White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Camp Fires Off-White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Shrigley Weed Off White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Shrigley Microwave Off White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Shirt Marstrand Palm Leaves Green",
+      "price": 89
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Shrigley Dodo Off White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Shrigley Bee Off White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm No Soup White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Phoney Date",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm George Black",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Brush Waves Oat White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Board Shorts Toroe Black",
+      "price": 79
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Swim Shorts Sandhamn Dusty Yellow",
+      "price": 59
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Shirt Marstrand Zebra Blue",
+      "price": 89
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Shirt Brantevik Work Stripe Dark Blu",
+      "price": 89
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Shirt Brantevik Navy",
+      "price": 89
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Polo Shirt Vaxholm Stormy Blue",
+      "price": 59
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Swim Shorts Sandhamn Coral Gems Dusty Yellow",
+      "price": 69
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Shirt Marstrand Stripes Multi Color",
+      "price": 79
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Swim Shorts Sandhamn Painted Palmtrees Black",
+      "price": 69
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Swim Shorts Sandhamn Sea Green",
+      "price": 59
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Swim Shorts Sandhamn Seabirds Sunset Multi Color",
+      "price": 69
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Swim Shorts Sandhamn Sodalite Blue",
+      "price": 59
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Camp Fires Off-White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm All Out Boat Off-White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Base Dusty Yellow",
+      "price": 29
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Sweatshirt Malmoe Base Dusty Yellow",
+      "price": 69
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Base Cashmere Pink",
+      "price": 29
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Sweatshirt Malmoe Base Cashmere Pink",
+      "price": 69
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "T-shirt Stockholm Shrigley Weed Off White",
+      "price": 39
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Shirt Varberg Sea Turtles Off-White",
+      "price": 89
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Swim Shorts Sandhamn Ginger Red",
+      "price": 59
+    },
+    {
+      "brand": "DedicatedBrand",
+      "name": "Shirt Varberg Burgundy",
+      "price": 69
+    },
+
+    {
+        "brand": "Montlimart",
+        "name": "SWEAT FUSION",
+        "color": "VERT",
+        "price": 105
+      },
+      {
+        "brand": "Montlimart",
+        "name": "SWEAT CLIMAT",
+        "color": "MOUTARDE",
+        "price": 95
+      },
+      {
+        "brand": "Montlimart",
+        "name": "POLO DUNE",
+        "color": "MARRON /BLANC /BEIGE",
+        "price": 95
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT INITIAL",
+        "color": "BEIGE",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT ESSAIM",
+        "color": "BLANC",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "SWEAT CAMPUS",
+        "color": "VERT",
+        "price": 100
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT INITIAL",
+        "color": "BLANC",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "CHEMISE CENTRE-LIN",
+        "color": "BLEU CLAIR RAYURES",
+        "price": 100
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT IDEAL",
+        "color": "GRIS CLAIR",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT IDEAL",
+        "color": "BLEU MARINE",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "CHEMISE MONTLIMART",
+        "color": "BLANC",
+        "price": 95
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT CITADIN",
+        "color": "BLANC",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "POLO MONTPOLO",
+        "color": "BEIGE",
+        "price": 75
+      },
+      {
+        "brand": "Montlimart",
+        "name": "CHEMISE SAINT PIERRE",
+        "color": "BLEU CIEL RAYURES",
+        "price": 150
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT IDEAL",
+        "color": "ROUGE",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "CHEMISE CENTRE BOURG",
+        "color": "BLANC",
+        "price": 105
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT ESSAIM",
+        "color": "ANTHRACITE",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT LIGUE",
+        "color": "BLEU CLAIR",
+        "price": 70
+      },
+      {
+        "brand": "Montlimart",
+        "name": "CHEMISE SURTOUT",
+        "color": "BLEU CLAIR",
+        "price": 75
+      },
+      {
+        "brand": "Montlimart",
+        "name": "POLO AMICAL",
+        "color": "MARINE",
+        "price": 80
+      },
+      {
+        "brand": "Montlimart",
+        "name": "SWEAT MI-TEMPS",
+        "color": "ROUGE",
+        "price": 110
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT ESSAIM",
+        "color": "ROUGE",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT BLASON",
+        "color": "GRIS CHINE",
+        "price": 65
+      },
+      {
+        "brand": "Montlimart",
+        "name": "CHEMISE CENTRE-VILLE",
+        "color": "TAUPE RAYURES",
+        "price": 115
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT SYMBOLE",
+        "color": "KAKI CLAIR",
+        "price": 70
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT BLASON",
+        "color": "GRIS CHINE",
+        "price": 65
+      },
+      {
+        "brand": "Montlimart",
+        "name": "CHEMISE PAULIN",
+        "color": "BLEU CLAIR",
+        "price": 150
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT INITIAL",
+        "color": "VERT",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT INITIAL",
+        "color": "BLEU ELECTRIQUE",
+        "price": 55
+      },
+      {
+        "brand": "Montlimart",
+        "name": "SWEAT DETENTE",
+        "color": "BEIGE",
+        "price": 115
+      },
+      {
+        "brand": "Montlimart",
+        "name": "T-SHIRT RESPECT",
+        "color": "VERT",
+        "price": 75
+      },
+      {
+        "brand": "Montlimart",
+        "name": "POLO RENAISSANCE",
+        "color": "KAKI",
+        "price": 85
+      },
+
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "HAUT SANS MANCHE MUSCLE TEE QUICK DRY",
+        "color": "MIST BLUE",
+        "price": 59
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "T-SHIRT ICONIC PLAYGROUND",
+        "color":"SAGE GREEN",
+        "price": 69
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "T-SHIRT TECHNIQUE AGILITY",
+        "color":"MIST BLUE",
+        "price": 69
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "T-SHIRT TECHNIQUE AGILITY",
+        "color": "GRANITE GREY",
+        "price": 69
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "T-SHIRT DE SPORT ICONIC POP",
+        "color":"NAVY",
+        "price": 69
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "T-SHIRT DE SPORT ICONIC POP",
+        "color": "MINTY FRESH",
+        "price": 69
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "HAUT SANS MANCHE MUSCLE TEE",
+        "color": "MOSS",
+        "price": 59
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "HAUT SANS MANCHE MUSCLE TEE",
+        "color": "BURGUNDY RED",
+        "price": 59
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "HAUT SANS MANCHE MUSCLE TEE",
+        "color": "ONYX BLACK",
+        "price": 59
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "HAUT SANS MANCHE MUSCLE TEE",
+        "color": "SAND",
+        "price": 59
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "T-SHIRT DE SPORT ICONIC",
+        "color": "NAVY",
+        "price": 65
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "T-SHIRT DE SPORT ICONIC",
+        "color": "MOSS",
+        "price": 65
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "T-SHIRT DE SPORT ICONIC",
+        "color": "SAND",
+        "price": 65
+      },
+      {
+        "brand": "Circle Sportswear",
+        "name_product": "T-SHIRT DE SPORT ICONIC",
+        "color": "LILAC",
+        "price": 65
+      }
+  ]
